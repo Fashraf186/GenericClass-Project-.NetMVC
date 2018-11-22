be8f102cb2a2ebf0ef1e177ec4c092a804467eb3
fashraf186 <fashraf@xavor.com>
2018-11-22 14:59:58 +0500

    Successfully implement the edit, create and delete method with views in Enrollment Controller.

--- GenericClass/GenericClass/Controllers/EnrollmentsController.cs
+++ GenericClass/GenericClass/Controllers/EnrollmentsController.cs
@@ -39,22 +39,30 @@ namespace GenericClass.Controllers
         // GET: Enrollments/Create
         public ActionResult Create()
         {
-            
+
+            ViewBag.CourseID = new SelectList(_context.Courses, "CourseID", "Title");
+            ViewBag.StudentID = new SelectList(_context.Students, "StudentID", "LastName");
             return View();
         }
 
         // POST: Enrollments/Create
         [HttpPost]
-        public ActionResult Create(FormCollection collection)
+        public ActionResult Create(Enrollment collection)
         {
             try
             {
                 // TODO: Add insert logic here
 
+                Eobj.Insert(collection);
+                Eobj.save();
                 return RedirectToAction("Index");
             }
+
+
             catch
             {
+                //ViewBag.CourseID = new SelectList(_context.Courses, "CourseID", "Title", .CourseID);
+                //ViewBag.StudentID = new SelectList(_context.Students, "StudentID", "LastName", enrollment.StudentID);
                 return View();
             }
         }
@@ -62,8 +70,12 @@ namespace GenericClass.Controllers
         // GET: Enrollments/Edit/5
         public ActionResult Edit(int id)
         {
-            
-            return View();
+
+            Enrollment e = Eobj.FindbyID(id);
+
+            ViewBag.CourseID = new SelectList(_context.Courses, "CourseID", "Title", e.CourseID);
+            ViewBag.StudentID = new SelectList(_context.Students, "StudentID", "LastName", e.StudentID);
+            return View(e);
         
             
         }
@@ -87,17 +99,22 @@ namespace GenericClass.Controllers
         // GET: Enrollments/Delete/5
         public ActionResult Delete(int id)
         {
-            return View();
+            Enrollment e = Eobj.FindbyID(id);
+
+            ViewBag.CourseID = new SelectList(_context.Courses, "CourseID", "Title", e.CourseID);
+            ViewBag.StudentID = new SelectList(_context.Students, "StudentID", "LastName", e.StudentID);
+            return View(e);
         }
 
         // POST: Enrollments/Delete/5
         [HttpPost]
-        public ActionResult Delete(int id, FormCollection collection)
+        public ActionResult Delete(int id, Enrollment collection)
         {
             try
             {
-                // TODO: Add delete logic here
-
+                
+                Eobj.Remove(id);
+                Eobj.save();
                 return RedirectToAction("Index");
             }
             catch
